fb96ec6be0918a65cc5add17fa27b1c91cff0c35
Альтаир <altair.botaguzov@gmail.com>
2017-11-18 08:55:45 +0600

    Olimpiada zadacha1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,42 @@
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int l,w,h,x;
+
         //FileReader fr = new FileReader("input.txt");
       //  FileWriter fw = new FileWriter(new File("output.txt"));
-        Scanner sc=new Scanner(System.in);
-        l=sc.nextInt(); w=sc.nextInt(); h=sc.nextInt();
-        x=2*(l*h+w*h);
-        if (x>=16) {
-            x=x/16;
-        } else if (x<16 && x>0) x=1;
-        System.out.println(Math.(x));
+
+
+
+        }
+
        // fw.close();
 
+
+    }
+
+class JavaArraySort {
+
+    public static void main(String[] args) throws IOException {
+        int R, N, i;
+
+
+        Scanner sc = new Scanner(System.in);
+        R = sc.nextInt();
+        N = sc.nextInt();
+        int arr[] = new int[N];
+        for (i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        //сортируем массив чисел
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
 
     }
 }
